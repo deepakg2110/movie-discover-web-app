2c4de725804bef87994c898004e25c60a4a41b73
deepakg2110 <32452533+deepakg2110@users.noreply.github.com>
2025-05-20 00:43:40 +0530

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,47 @@
+// Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+
+// Mock IntersectionObserver
+class MockIntersectionObserver {
+  constructor(callback) {
+    this.callback = callback
+    this.elements = new Set()
+    this.observe = jest.fn((element) => {
+      this.elements.add(element)
+      this.callback([{ isIntersecting: true, target: element }])
+    })
+    this.unobserve = jest.fn((element) => {
+      this.elements.delete(element)
+    })
+    this.disconnect = jest.fn()
+  }
+}
+
+window.IntersectionObserver = MockIntersectionObserver
+
+// Mock matchMedia
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
+// Mock ResizeObserver
+class MockResizeObserver {
+  constructor(callback) {
+    this.callback = callback
+    this.observe = jest.fn()
+    this.unobserve = jest.fn()
+    this.disconnect = jest.fn()
+  }
+}
+
+window.ResizeObserver = MockResizeObserver
